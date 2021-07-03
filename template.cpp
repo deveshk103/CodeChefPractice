03d576c549e9d8728be2a50fbff56e80aae2f326
Devesh <66843808+deveshk103@users.noreply.github.com>
2021-07-03 13:02:24 +0530

    Solution of all the questions that I have practiced

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+
+    for(int i = 0; i < t; i++)
+    {
+        int n;
+        cin>>n;
+        cout<<n<<endl;
+    }
+    return 0;
+}
