94703a5484dc55fe9e9aeb16c4bfd1e8d67ac639
deveshk103 <66843808+deveshk103@users.noreply.github.com>
2021-09-02 22:44:10 +0530

    Update thankschef.cpp

--- thankschef.cpp
+++ thankschef.cpp
@@ -2,32 +2,27 @@
 
 using namespace std;
 
-void thankschef(void){
-	cout << "Thanks for helping Chef!"<<endl;
+void thankschef(void) {
+  cout << "Thanks for helping Chef!" << endl;
 }
 
-int main()
-{
-	int t;
-	cin >> t;
+int main() {
+  int t;
+  cin >> t;
 
-	if (t >= 1 && t <= 1000)
-	{
-		for (int i = 0; i < t; i++)
-		{
-			int n;
-			cin >> n;
+  if (t >= 1 && t <= 1000) {
+    for (int i = 0; i < t; i++) {
+      int n;
+      cin >> n;
 
-			if (n<=20 && n>= -20)
-			{
-				if (n<10){
-					thankschef();
-				}
-				else{
-					cout << "-1"<<endl;
-				}
-			}
-		}
-	}
-	return 0;
+      if (n <= 20 && n >= -20) {
+        if (n < 10) {
+          thankschef();
+        } else {
+          cout << "-1" << endl;
+        }
+      }
+    }
+  }
+  return 0;
 }
