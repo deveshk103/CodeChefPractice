a8b61a7f82b3aae65f0ff67bcaf1329c4d72c784
deveshk103 <66843808+deveshk103@users.noreply.github.com>
2021-09-02 22:46:21 +0530

    Update secondlargest.cpp

--- secondlargest.cpp
+++ secondlargest.cpp
@@ -2,22 +2,21 @@
 
 using namespace std;
 
-int main(){
-	int t;
-	cin>>t;
-	int arr[3];
-	float result;
-	for(int i=0;i<t;i++)
-	{	int j;
-		for(j=0; j<3;j++){
-			
-			cin>>arr[j];
-		}
-		sort(arr,arr+j);
-		cout<<arr[1]<<endl;
+int main() {
+  int t;
+  cin >> t;
+  int arr[3];
+  float result;
+  for (int i = 0; i < t; i++) {
+    int j;
+    for (j = 0; j < 3; j++) {
 
-	
-	}
+      cin >> arr[j];
+    }
+    sort(arr, arr + j);
+    cout << arr[1] << endl;
 
-	return 0;
+  }
+
+  return 0;
 }
